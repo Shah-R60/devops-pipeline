e5dfe0c35f437c52170f3c203ede29c5110be25d
rahul <shahrahul3600@gmail.com>
2026-05-31 14:45:34 +0530

    add ci pipeline

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log('Hello, World!');
+const name = 'Alice';
+console.log(`Hello, ${name}!`);
+console.log("i am learning ci/cd github actions");
---
+++ test.js
@@ -0,0 +1,6 @@
+console.log('starting test!');
+setTimeout(() => {
+    console.log('This is a delayed message.');
+}, 3000);
+console.log('test complete !.');
+
